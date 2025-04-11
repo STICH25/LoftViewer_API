b710e59c3ce73dcfef627e98f15b54bcbaa85d9a
Jaylom Pairol <jaylotom10@gmail.com>
2025-04-11 17:15:20 -0400

    dd

--- obj/Debug/net8.0/LoftViewer.AssemblyInfo.cs
+++ obj/Debug/net8.0/LoftViewer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoftViewer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96a38e99b39707263ada1f8512ec89dcca71fcf7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a975faaad563cd9e10bd18d654ce75cecb5360fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoftViewer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoftViewer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
